560a440341a732aafdc305fcc51a4c2735f75a3f
ocyt3 <159497908+ocyt3@users.noreply.github.com>
2024-02-09 01:02:45 +0100

    Add dgg_kick_html5_controls.js

---
+++ dgg_kick_html5_controls.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        DGG Kick HTML5 controls
+// @namespace   Violentmonkey Scripts
+// @match       *://*player.kick.com/*
+// @grant       none
+// @version     1.0
+// @author      ocyt3
+// @description 12/15/2023, 12:44:42 AM
+// ==/UserScript==
+
+async function waitForElement(selector, timeout = 15000) {
+  const start = Date.now();
+
+  while (Date.now() - start < timeout) {
+    const el = document.querySelector(selector);
+    if (el) {
+      return el;
+    }
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
+  return null;
+}
+
+waitForElement(".video-container__controls").then(elem => {
+  elem.style.display = "none";
+});
+
+waitForElement("video").then(elem => {
+  elem.setAttribute("controls", "true")
+  console.log(elem);
+})
